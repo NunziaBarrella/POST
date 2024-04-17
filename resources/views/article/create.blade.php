dda7ec814c4a79b3d89346db72bda4d8090c9ada
NunziaBarrella <nunzia.barrella@gmail.com>
2024-04-17 15:58:40 +0200

    form_articles

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 <div class="container-fluid p-5 bg-info text-center text-white">
         <div class="row justify-content-center">
-        <h1 class="dispaly-1">The Aulab Post</h1>
+        <h1 class="dispaly-1">Inserisci un articolo</h1>
         </div>
     </div>
     <div class="container my-5">
@@ -21,7 +21,33 @@
 
                     <div class="mb-3">
                         <label for="title" class="form-label">Titolo:</label>
-                        <input type="text" name="title" class="form-control" id="title" value={{old}}>
+                        <input type="text" name="title" class="form-control" id="title" value="{{old('title')}}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="subtitle" class="form-label">Sottotilo:</label>
+                        <input type="text" name="subtitle" class="form-control" id="subtitle" value="{{old('subtitle')}}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Immagine:</label>
+                        <input type="file" name="image" class="form-control" id="image">
+                    </div>
+                    <div class="mb-3">
+                        <label for="category" class="form-label">Categoria:</label>
+                        <select name="category" id="category" class="form-control text-capitalize">
+                            @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                    <label for="body" class="form-label">Testo:</label>
+                    <textarea name="body" id="body" cols="30" rows="7" class="form-control">{{old ('body')}}
+                    </textarea>
+                    </div>
+
+                    <div class="mt-2 d-flex justify-content-center">
+                        <button class="btn btn-info text-white">Inserisci un articolo</button>
+                    </div>
                     </div>
                 </form>
             </div>
